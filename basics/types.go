3ad880706e59f82205495a0e0ef3aeb644e1b8e0
Sid Sachdev <sid.sachdev9@gmail.com>
2020-01-02 05:31:29 -0500

    Updated types, created new type

--- basics/types.go
+++ basics/types.go
@@ -5,10 +5,27 @@ import "fmt"
 var y = 42
 var a = "car"
 
+// define new type
+type catname string
+
 func main() {
-	fmt.Printf("%T\n", y)
-	fmt.Println(y)
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
 	//	y = "car"
 	//	fmt.Println(y)
 	// SINCE GO is STATIC typed this will throw an error
+
+	// working on newly created type
+	name := catname("Maya")
+	fmt.Println(name)
+	fmt.Printf("%T\n", name)
+
+	// a = name
+	// WILL NOT WORK
+
+	//conversion NOT casting
+	// lets convert catname to string
+	a = string(name)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
 }
