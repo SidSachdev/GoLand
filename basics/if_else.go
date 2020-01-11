b65fb01bfeb4ee1adec25975f6ea972724595ed7
Sid Sachdev <sid.sachdev9@gmail.com>
2020-01-11 12:53:19 -0500

    Added if else and else if

---
+++ basics/if_else.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x:=41
+	if x == 40{
+		fmt.Println("Value was 40")
+	} else if x == 41{
+		fmt.Println("Value was 41")
+	} else {
+		fmt.Println("Value was not 40")
+	}
+}
