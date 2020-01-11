45412c75cfe5f0049c7731689c55bc270eb848eb
Sid Sachdev <sid.sachdev9@gmail.com>
2020-01-11 12:28:49 -0500

    Added nested loops

---
+++ basics/nested_loop.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For loop in Golang
+	// for init; condition; post {}
+	for i := 0; i <= 10; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("The outer loop: %d \t The inner loop: %d\n", i, j)
+		}
+ 	}
+}
