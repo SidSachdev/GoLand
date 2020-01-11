a02a089391a36bcacb7afa516f963c2ab1e26879
Sid Sachdev <sid.sachdev9@gmail.com>
2020-01-11 12:23:46 -0500

    Added control flow

---
+++ basics/control_flow.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For loop in Golang
+	// for init; condition; post {}
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0{
+			fmt.Println(i)
+		}
+ 	}
+}
